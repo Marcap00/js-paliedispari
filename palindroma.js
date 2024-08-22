c00aabff0e12af4315162a047d98b2123301d74d
Marcap00 <cappellatomarco@yahoo.com>
2024-08-22 15:23:33 +0200

    first commit

---
+++ palindroma.js
@@ -0,0 +1,16 @@
+/* 
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/* 
+1- Recuperiamo gli elementi dal DOM
+2- Recuperiamo l'input dell'utente
+3- Mettiamo in ascolto sugli eventi il form
+4- Blocchiamo il comportamento di base del form
+5- Creiamo una funzione che verifichi se la parola è palindroma
+    -rigiriamo la parola
+    -verifichiamo se la parola di origine e la parola inversa sono uguali
+    -se si è palindroma
+    -se no non è palindroma
+*/
